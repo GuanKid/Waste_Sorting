d384651f2f399afa19bc26810029fcbe36da3487
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2025-03-05 23:06:54 +0200

    Delete assessment directory

--- assessment/src/main/java/com/enviro/assessment/grad001/desiregwanzura/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.enviro.assessment.grad001.desiregwanzura.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
